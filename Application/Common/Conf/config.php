399629b86a0e1494c570e07051cf7581ac86f603
zhaobin <zhaobin@deepin.deepin-domain>
2015-01-21 10:41:03 +0800

    add

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,8 +1,8 @@
 <?php
 
 return array(
-    'DB_PREFIX' => 'hongbao_',
-    'DB_DSN' => 'mysql://root:820819@localhost:3306/hongbao',
+    'DB_PREFIX' => 'wxmodule_',
+    'DB_DSN' => 'mysql://root:820819@localhost:3306/wxmodule',
 
     'DEFAULT_FILTER' =>'htmlspecialchars,stripslashes',
     'ACTION_SUFFIX' => 'Action'
